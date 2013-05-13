ec50394c1d3893528b3caa8764c14ab19dced9ab
AlrikG <lord_alrik@web.de>
2013-05-13 13:50:36 +0200

    - add main instance

--- Statistics/src/de/esper/CEPListener.java
+++ Statistics/src/de/esper/CEPListener.java
@@ -1,11 +1,16 @@
 package de.esper;
+
 import com.espertech.esper.client.EventBean;
 import com.espertech.esper.client.UpdateListener;
+
+import de.core.Main;
 
 public class CEPListener implements UpdateListener
 {
 	public void update(EventBean[] newData, EventBean[] oldData)
 	{
 		System.out.println("Event received: " + newData[0].getUnderlying());
+
+		Main.main.getEventDecoder().getPlayerList();
 	}
 }
