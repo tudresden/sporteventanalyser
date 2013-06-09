e6d2f8304bd18458ac8a372f80a3659ead3826fa
Philipp <pg_@gmx.net>
2013-06-09 17:23:48 +0200

    prediction calculations

--- Predictions/src/predictions/GoalPredictor.java
+++ Predictions/src/predictions/GoalPredictor.java
@@ -26,16 +26,41 @@ public class GoalPredictor {
 		numberSamples = 0;
 	}
 
+	/**
+	 * @param event
+	 *            "loss" or "out of bounds" or "goal"
+	 */
 	public void onEvent(String event) {
 		Instance trainingInstance = instanceManager.getTrainingInstance(event);
 		learner.trainOnInstance(trainingInstance);
 
+		if (learner.correctlyClassifies(trainingInstance)) {
+			numberSamplesCorrect++;
+			System.out.println("[PREDICTION] prediction correct");
+		} else {
+			System.out.println("[PREDICTION] prediction false");
+		}
 		numberSamples++;
 	}
 
 	public void onPlayerChange(String nameOfPlayerWithBall) {
+
 		// count ball possession of player
 		instanceManager.updateInstance(nameOfPlayerWithBall);
+
+		// predict
+		double[] predictions = learner.getVotesForInstance(instanceManager
+				.getInstanceForPrediction());
+
+		if (predictions.length == 3) {
+			double sum = predictions[0] + predictions[1] + predictions[2];
+			System.out.println("[PREDICTION] " + sum + " votes:  "
+					+ predictions[0] / sum * 100 + "% goal  -  "
+					+ predictions[1] / sum * 100 + "% ball loss  -  "
+					+ predictions[2] / sum * 100 + "% out of bounds");
+
+		} else
+			System.out.println("[PREDICTION] Votes: - n/a -");
 
 	}
 }
