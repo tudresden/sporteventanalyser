cfea6c040e5242bf01ea2798921ac4325bcd1b93
Philipp <pg_@gmx.net>
2013-06-09 16:35:17 +0200

    classes for prediction test

---
+++ Predictions/src/predictions/GoalPredictor.java
@@ -0,0 +1,15 @@
+package predictions;
+
+public class GoalPredictor {
+
+	public void onEvent(String event) {
+		// goal / ball loss / ball out of bounds
+		// TODO train the learner with the event
+
+	}
+
+	public void onPlayerChange(String nameOfPlayerWithBall) {
+		// TODO count ball possession of player and test prediction
+
+	}
+}
