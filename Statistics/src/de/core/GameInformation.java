c8adce1e5160443e9298f6538bdf9445de1d9496
Tommy Kubica <tommy_1991@web.de>
2013-06-12 16:40:28 +0200

    -Add Team Contacts

--- Statistics/src/de/core/GameInformation.java
+++ Statistics/src/de/core/GameInformation.java
@@ -2,11 +2,13 @@ package de.core;
 
 public class GameInformation
 {
+	//Ball
 	public int getActiveBallId()
 	{
 		return Main.main.currentActiveBallId;
 	}
 
+	//Player
 	public float getPlayerDistance(int id)
 	{
 		return Main.main.getEntityFromId(id).getTotalDistance();
@@ -49,6 +51,21 @@ public class GameInformation
 		{
 			return -1;
 		}
+	}
+	
+	//Team
+	private int[] a = {47, 49, 19, 53, 23, 57, 59}; //vorerst ohne Torwart
+	private int[] b = {63, 65, 67, 69, 71, 73, 75}; //vorerst ohne Torwart
+	
+	//type a for team A, b for team B
+	public int getTeamContacts(int[] teamkuerzel)
+	{
+		int contacts = 0;
+		for(int i=0; i<teamkuerzel.length; i++)
+		{
+			contacts+=getPlayerBallContacts(teamkuerzel[i]);
+		}
+		return contacts;
 	}
 }
 
