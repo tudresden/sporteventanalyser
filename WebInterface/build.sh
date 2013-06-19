fb1d00536ccc172b01392b7b585e48e931344f47
Peter Schwede <Peter.Schwede@gmx.net>
2013-06-19 16:02:39 +0200

    command to convert; optional argument to build all and convert images

--- WebInterface/build.sh
+++ WebInterface/build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if [[ "$1" == "help" ]]; then
+  echo "USAGE:"
+  echo
+  echo "$0 all    -   build everything"
+  echo "$0 img    -   convert images aswell"
+  echo "$0 help   -   show this help"
+  exit 0
+fi
+
 mkdir -p ./js
 coffee -j js/engine.js -c coffee/*.coffee || exit 1
 lessc --yuicompress less/style.less css/style.css
+
+if [[ "$1" == "all" ]] || [[ "$1" == "img" ]]; then
+  cd img
+  for f in $(ls *.svg | grep -o "^[^\.]\+"); do inkscape -D -e "$f.png" -f "$f.svg"; done
+  cd ..
+fi
