4db2c8fe2b16d22d4af0a8536cfdd4bbebb7b8bb
SEArichard <kpiir13>
2013-06-20 19:59:56 +0200

    - new class to encapsulate data needed for creating new objects of class HeatMapGrid - just a record of values, no functionality

---
+++ Statistics/src/de/core/HeatMapInit.java
@@ -0,0 +1,12 @@
+package de.core;
+
+public class HeatMapInit
+{
+
+	public int widthInCells;
+	public int heightInCells;
+	public float widthResolution;
+	public float heightResolution;
+	public int yMinNegativeAbs;
+	public int xMinNegativeAbs;
+}
