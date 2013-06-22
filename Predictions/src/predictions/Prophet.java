a49af0dbf0f8be75fa5cadedb8fb09e26573d5f6
Philipp <pg_@gmx.net>
2013-06-22 12:34:13 +0200

    prediction updates

--- Predictions/src/predictions/Prophet.java
+++ Predictions/src/predictions/Prophet.java
@@ -2,12 +2,15 @@ package predictions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import de.core.GameInformation;
 
 public class Prophet {
 	private List<Predictor> listOfPredictors;
 	private GameInformation gameInformation;
+	private Timer timer;
 
 	public Prophet(GameInformation gameInformation) {
 		this.gameInformation = gameInformation;
@@ -15,11 +18,36 @@ public class Prophet {
 		listOfPredictors = new ArrayList<Predictor>();
 	}
 
+	/**
+	 * Start the periodic predictions.
+	 */
 	public void start() {
 		System.out.println("Prophet awakened.");
+
+		if (timer != null)
+			timer.cancel();
+
+		timer = new Timer();
+
+		timer.schedule(new TimerTask() {
+			public void run() {
+				updatePredictors();
+			}
+		}, 0, 3 * 1000);
 	}
 
+	/**
+	 * Stop the periodic predictions.
+	 */
 	public void stop() {
+		if (timer != null)
+			timer.cancel();
+	}
 
+	private void updatePredictors() {
+		System.out.println("Updating predictors.");
+
+		for (Predictor predictor : listOfPredictors)
+			predictor.update(gameInformation);
 	}
 }
