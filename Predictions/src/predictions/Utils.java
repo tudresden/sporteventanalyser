29b168afab7d876915bddacadd587f82d1146be9
Onur Ekici <ekicion@gmail.com>
2013-06-24 13:55:24 +0200

    added log function for evolutions

--- Predictions/src/predictions/Utils.java
+++ Predictions/src/predictions/Utils.java
@@ -1,11 +1,18 @@
 package predictions;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import weka.core.Instance;
+
 public class Utils {
 
 	public static final String FIELD_AREA_OWN_TEAM = "FIELD_AREA_OWN_TEAM";
 	public static final String FIELD_AREA_MIDDLE = "FIELD_AREA_MIDDLE";
 	public static final String FIELD_AREA_OPPONENTS = "FIELD_AREA_OPPONENTS";
-
+	public int single=0;
+	
 	public static final String getFieldArea(int x, boolean onOpponentSide) {
 		final int totalWidth = 2 * 52500;
 
@@ -25,4 +32,66 @@ public class Utils {
 		}
 
 	}
+	
+	public void logArff(Instance predictionInstance){
+		try {
+			PrintWriter out = new PrintWriter(new FileWriter("log.dataset.arff",true));
+			String str="";
+			String header="";
+
+			int max=predictionInstance.numAttributes();
+			
+			for(int i=0;i<max ;i++){
+				if(this.single==0)
+					header+="@Attribute "+predictionInstance.attribute(i).name()+"\n";
+				str+=predictionInstance.attribute(i).value(0)+",";
+
+			}
+			if(this.single==0){
+				out.write(header+"\n");
+				this.single=1;
+			}
+			
+			str+="\n";
+			out.write(str);
+			out.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void logInt(Integer dataSet[]){
+		try {
+			PrintWriter out = new PrintWriter(new FileWriter("log.dataset.csv",true));
+			String str="";
+			String header="";
+			int counter=0;
+			for(int data : dataSet){
+				if(this.single==0)
+					header+="data"+counter+",";
+				str+=data+",";
+				counter++;
+			}
+			if(this.single==0){
+				out.write(clean(header)+"\n");
+				this.single=1;
+			}
+			
+			str+="\n";
+			out.write(clean(str));
+			out.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public String clean(String str) {
+
+		  if (str.length() > 0) {
+		    str = str.substring(0, str.length()-1);
+		  }
+		  return str;
+		}
+	
+	
 }
